cbbe3a560f49560495d7791ded08180ca88fd521
ElCreaturoM50S <michal.szewczuk@cyfroweszkoly.pl>
2022-12-15 12:33:00 +0100

    Add simple stuff :+1:

---
+++ main.py
@@ -0,0 +1,14 @@
+import customtkinter
+
+class Root(customtkinter.CKt):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("300x300")
+        self.title("snejk")
+
+        self.gameMenu = None
+        self.gameMain = None
+        
+root = Root()
+root.mainloop()
